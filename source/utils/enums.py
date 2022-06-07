159691ade37d0d1ead50dd4be1ee4cc5075f566e
Damego <danyabatueff@gmail.com>
2022-06-07 14:22:58 +0500

    feat!: Поддержка манг с несколькими переводами (#3)

--- source/utils/enums.py
+++ source/utils/enums.py
@@ -1,12 +1,6 @@
 from enum import IntEnum
 
 
-class DiscordColors(IntEnum):
-    BLURPLE = 0x5865F2
-    GREEN = 0x57F287
-    YELLOW = 0xFEE75C
-    FUCHSIA = 0xEB459E
-    RED = 0xED4245
-    WHITE = 0xFFFFFF
-    BLACK = 0x000000
-    EMBED_COLOR_DARK = 0x2F3136
+class IDS(IntEnum):
+    GUILD_ID = 822119465575383102
+    CHANNEL_ID = 969464310294278214
--- source/utils/mangalib.py
+++ source/utils/mangalib.py
@@ -1,5 +1,14 @@
+import datetime
+from typing import List, Dict
+
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+
+time_format = "%d.%m.%Y"
+xpath = '//*[@id="main-page"]/div/div/div/div[2]/div[2]/div[3]/div/div[1]/div[1]/div[2]/div[{i}]'
 
 
 def load_manga_data(driver: webdriver.Chrome, name: str):
@@ -9,43 +18,35 @@ def load_manga_data(driver: webdriver.Chrome, name: str):
     driver.get(url=search_url)
     try:
         url = driver.find_element(By.CLASS_NAME, "media-card").get_attribute("href")
-    except Exception:
+    except NoSuchElementException:
         return
+    driver.get(url=url)
 
     # Getting data of manga
-    driver.get(url=f"{url}?section=chapters")
-
     score_data = driver.find_element(
         By.CLASS_NAME, "media-rating-wrap"
     ).text.splitlines()
     score, count = score_data[0], score_data[1]
-
     data_raw = driver.find_element(By.CLASS_NAME, "media-info-list").text.splitlines()
     data = {data_raw[i]: data_raw[i + 1] for i in range(0, len(data_raw) - 1, 2)}
-
     image_card = driver.find_element(By.CLASS_NAME, "media-sidebar__cover")
     image_url = image_card.find_element(By.XPATH, "img").get_attribute("src")
-    print(image_url)
-
     full_name = driver.find_element(By.CLASS_NAME, "media-name__main").text
-
     first_chapter_url = (
         driver.find_element(By.CLASS_NAME, "media-sidebar__buttons")
         .find_element(By.XPATH, "a")
         .get_attribute("href")
     )
-    last_chapter_data = driver.find_element(
-        By.CLASS_NAME, "media-chapter__name"
-    ).find_element(By.CLASS_NAME, "link-default")
-    last_chapter_name = last_chapter_data.text
-    last_chapter_url = last_chapter_data.get_attribute("href")
-
     driver.get(url=f"{url}?section=info")
-
     description = driver.find_element(By.CLASS_NAME, "media-description__text").text
     genres = driver.find_element(By.CLASS_NAME, "media-tags").text.splitlines()
 
-    data = {
+    driver.get(url=f"{url}?section=chapters")
+    new_url, last_chapter_data = _parse_last_chapter(driver)
+    if new_url is not None:
+        url = new_url.split("?")[0]
+
+    return {
         "name": full_name,
         "description": description,
         "image_url": image_url,
@@ -54,21 +55,53 @@ def load_manga_data(driver: webdriver.Chrome, name: str):
         "url": url,
         "score_info": {"score": score, "count": count},
         "first_chapter_url": first_chapter_url,
-        "last_chapter": {"name": last_chapter_name, "url": last_chapter_url},
+        "last_chapter": last_chapter_data,
     }
-    return data
+
+def _parse_last_chapter(driver: webdriver.Chrome, url: str = None):
+    try:
+        chapter_data = driver.find_element(
+            By.CLASS_NAME, "media-chapter__name"
+        )
+        chapter_data = chapter_data.find_element(By.CLASS_NAME, "link-default")
+        chapter_name = chapter_data.text
+        chapter_url = chapter_data.get_attribute("href")
+    except Exception: # У манги несколько переводов, поэтому страница пустая
+        new_url = get_new_url(driver)
+        return _parse_last_chapter(driver, new_url)
+    return url, {"name": chapter_name, "url": chapter_url}
+
+def get_new_url(driver: webdriver.Chrome):
+    team_list = driver.find_element(By.CLASS_NAME, "team-list")
+    teams: List[WebElement] = team_list.find_elements(By.CLASS_NAME, "team-list-item") # Ищем все переводы
+    data = {}
+    for i in range(len(teams)):
+        team = team_list.find_element(By.XPATH, xpath.format(i=i+1)) # Получаем элемент через XPATH, т.к. иначе на него нельзя нажать
+        team.click()
+        chapter_data = get_all_chapter_data(driver)
+        time = chapter_data.text.splitlines()[2]
+        dtime = datetime.datetime.strptime(time, time_format)
+        data[dtime] = driver.current_url
+
+    return get_actual_translate(data)
+
+
+def get_actual_translate(data: Dict[datetime.datetime, str]):
+    actual = max(data)
+    return data[actual]
+
+
+def get_all_chapter_data(driver: webdriver.Chrome):
+    return driver.find_element(
+        By.CLASS_NAME, "media-chapter"
+    )
 
 
 def check_new_chapter(driver: webdriver.Chrome, url: str):
+    if "section=chapters" not in url:
+        url += "?section=chapters"
     driver.get(url)
-    chapter_data = driver.find_element(
-        By.CLASS_NAME, "media-chapter__name"
-    ).find_element(By.CLASS_NAME, "link-default")
-    chapter_name = chapter_data.text
-    chapter_url = chapter_data.get_attribute("href")
-
-    data = {"name": chapter_name, "url": chapter_url}
-    return data
+    return _parse_last_chapter(driver)
 
 
 def base64_from_url(driver: webdriver.Chrome, url: str):
