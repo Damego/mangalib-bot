9888a60b523de294c61dd12e8064c8b71062085f
Damego <danyabatueff@gmail.com>
2022-06-07 14:20:17 +0500

    Delete test2.py

--- test2.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-raw_time1 = "04.05.22"
-raw_time2 = "28.08.01"
-time_format = "%d.%m.%y"
-time1 = datetime.datetime.strptime(raw_time1, time_format)
-time2 = datetime.datetime.strptime(raw_time2, time_format)
-times = {"1": time1, "2": time2}
-
